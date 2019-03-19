b84b380f1a5c51f86cbbe8131167c528cabb8bd2
Ragil Triatmojo <ragil@redpedia.com>
2019-03-19 14:50:39 +0700

    Pengenalan Class dan Object

---
+++ Mahasiswa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClass
+{
+    class Mahasiswa
+    {
+        //Properties
+        public string nim { get; set; }
+        public string nama { get; set; }
+        public float ipk { get; set; }
+
+        //Method
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama : {0}", nama);
+            Console.WriteLine("NIM : {0}", nim );
+            Console.WriteLine("IPK : {0}", ipk);
+            Console.WriteLine("Telah melakukan registrasi");
+        }
+
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses KRS\n", nama);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            //set nilai properties
+            saya.nim = "2286";
+            saya.nama = "Ragil";
+            saya.ipk = 4;
+
+            kamu.nim = "9696";
+            kamu.nama = "Alex";
+            kamu.ipk = 4;
+            
+
+            //call method registrasi & isi KRS
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+
+
+        }
+
+    }
+}
